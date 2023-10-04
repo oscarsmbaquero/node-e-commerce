734ce4edb070ca4590dc11b92eea721eba494d9f
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2023-10-04 20:01:36 +0200

    inventario ok

--- server/api/controllers/products.controller.js
+++ server/api/controllers/products.controller.js
@@ -4,15 +4,12 @@ import { User } from "../models/User.Model.js";
 import { Ventas } from "../models/Ventas.Model.js";
 import { ObjectId } from "mongodb";
 
-
-
-
 const getProducts = async (req, res, next) => {
-  console.log('Entroxxxxxxxxxxxxx');
+  console.log("Entroxxxxxxxxxxxxx");
   try {
     const products = await Products.find();
     //.populate({ path: "numeroPedido", select: "unidades" });
-    
+
     // .populate({ path: "materialIntervencion",select: "descripcion"})
     // .populate({path:'cliente', select :""})
     return res.status(200).json(products);
@@ -45,9 +42,10 @@ const productsDetail = async (req, res, next) => {
     return next(error);
   }
 };
+// const buyProducts = async (req, res, next) => {
+//   //console.log(req.body);
+
 const buyProducts = async (req, res, next) => {
-  //console.log(req.body);
-  
   try {
     const orderNumber = generateOrderNumber(); // Generar un número de pedido único para la compra
     const userBuy = req.body.idUser;
@@ -59,34 +57,89 @@ const buyProducts = async (req, res, next) => {
         precio: productData.precio,
       };
     });
-
     // Crear una venta con el número de pedido
     const newSale = new Ventas({
       orderNumber: orderNumber,
       products: productsToInsert,
       userBuy: userBuy,
-      estadoPedido: 'Preparando pédido',
-      //buyerEmail: buyerEmail, // Agregar el correo electrónico del comprador
+      estadoPedido: "Preparando pedido",
     });
-    console.log(newSale.userBuy);
     // Guardar la venta en la base de datos
-    const newPedidoCliente =  await newSale.save();
+    const newPedidoCliente = await newSale.save();
     await User.updateOne(
       { _id: userBuy },
       { $push: { numeroPedido: newSale._id } },
       { new: true }
     );
-
-    // Puedes responder con el número de pedido si es necesario
+    // Itero por los productos para extraer el id y las unidades
+    for (const productData of req.body.products) {
+      const productId = productData._id;
+      const unidadesToSubtract = productData.unidades;
+      //Busco en la base de datos cada producto
+      const productoActual = await Products.findById(productId);
+      //el producto que encuentre con cada id
+      if (productoActual) {
+        // Le resto las unidades
+        const unidadesRestantes = productoActual.unidades - unidadesToSubtract;
+        // Actualizo las unidades en la base de datos
+        await Products.findByIdAndUpdate(productId, {
+          unidades: unidadesRestantes,
+        });
+      }
+    }
+    // Responder con el número de pedido si es necesario
     return res.status(201).json({
       status: 201,
-      message: `Venta registrada correctamente, su numero de pedidos es ${orderNumber}`,
+      message: `Venta registrada correctamente, su número de pedido es ${orderNumber}`,
       data: { orderNumber: orderNumber },
     });
   } catch (error) {
     return next(error);
   }
 };
+
+//   try {
+//     const orderNumber = generateOrderNumber(); // Generar un número de pedido único para la compra
+//     const userBuy = req.body.idUser;
+//     const productsToInsert = req.body.products.map((productData) => {
+//       return {
+//         name: productData.name,
+//         description: productData.description,
+//         unidades: productData.unidades,
+//         precio: productData.precio,
+//       };
+//     });
+
+//     // Crear una venta con el número de pedido
+//     const newSale = new Ventas({
+//       orderNumber: orderNumber,
+//       products: productsToInsert,
+//       userBuy: userBuy,
+//       estadoPedido: "Preparando pédido",
+//       //buyerEmail: buyerEmail, // Agregar el correo electrónico del comprador
+//     });
+//     console.log(newSale);
+//     // Guardar la venta en la base de datos
+//     const newPedidoCliente = await newSale.save();
+//     await User.updateOne(
+//       { _id: userBuy },
+//       { $push: { numeroPedido: newSale._id } },
+//       { new: true }
+//     );
+//     const estadoUpdated = await Products.findByIdAndUpdate(products._id, {
+//       unidades: products.unidades,
+//     });
+
+//     // Puedes responder con el número de pedido si es necesario
+//     return res.status(201).json({
+//       status: 201,
+//       message: `Venta registrada correctamente, su numero de pedidos es ${orderNumber}`,
+//       data: { orderNumber: orderNumber },
+//     });
+//   } catch (error) {
+//     return next(error);
+//   }
+// };
 
 // const buyProducts = async (req, res, next) => {
 //   try {
@@ -120,7 +173,7 @@ const buyProducts = async (req, res, next) => {
 
 /**
  * Generar numero de pedido por cada compra
- * @returns 
+ * @returns
  */
 function generateOrderNumber() {
   // Puedes usar una lógica personalizada para generar números de pedido únicos
@@ -129,59 +182,6 @@ function generateOrderNumber() {
   const randomPart = Math.floor(Math.random() * 1000); // Número aleatorio entre 0 y 999
   return `${timestamp}-${randomPart}`;
 }
-
-
-// const updateCars = async (req, res, next) => {
-//   console.log(req.body)
-//   try {
-//     const { id } = req.params;
-//     const update = req.body;
-//     const options = { new: true };
-//     const updatedCar = await Cars.findByIdAndUpdate(id, update, options);
-
-//     if (!updatedCar) {
-//       return res.status(404).json({
-//         status: 404,
-//         message: httpStatusCode[404],
-//         error: "Car not found",
-//       });
-//     }
-
-//     return res.status(200).json({
-//       status: 200,
-//       message: httpStatusCode[200],
-//       data: { car: updatedCar },
-//     });
-//   } catch (error) {
-//     return next(error);
-//   }
-// };
-
-// const deleteCar = async(req, res, next)=>{
-//   console.log('Entro');
-//   try {
-//     const { id } = req.params;
-   
-//     const carDelete = await Cars.findByIdAndDelete(id);
-
-    
-//     return res.json({
-//       status: 200,
-//       message: httpStatusCode[200],
-//       data: { car: carDelete },
-//     });
-//   } catch (error) {
-//     return next(error);
-//   }
-// }
-
-
-
-
-
-
-
-
 
 
 export { getProducts, productsDetail, buyProducts };
